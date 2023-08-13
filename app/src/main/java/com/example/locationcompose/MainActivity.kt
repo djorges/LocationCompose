ace6567a3e4c8435b59780404df9094069c8d13b
djorges <jorgesdiego@hotmail.com>
2023-08-13 18:47:52 -0300

    New Project

---
+++ app/src/main/java/com/example/locationcompose/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.locationcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import com.example.locationcompose.ui.theme.LocationComposeTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LocationComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Text(text = "Hello name!")
+                }
+            }
+        }
+    }
+}
